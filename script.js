ad99562aacf97ce3841df3ef84b7f8e41d3c5750
Quebvihn <bamedinatorres@gmail.com>
2023-09-30 20:51:36 -0400

    questions array

---
+++ script.js
@@ -0,0 +1,24 @@
+let containerQuestionEl = document.getElementById("qs-container");
+let containerStartEl = document.getElementById("start-container");
+let containerEndEl = document.getElementById("end-container")
+let containerScoreEl = document.getElementById("scores")
+let formInitials = document.getElementById("initials-form")
+let containerHighScoresEl = document.getElementById("high-score-container")
+let ViewHighScoreEl = document.getElementById("view-high-scores")
+let listHighScoreEl = document.getElementById("high-score-list")
+let correctEl = document.getElementById("correct")
+let wrongEl = document.getElementById("wrong")
+
+let btnStartEl = document.querySelector("#start-game");
+let btnGoBackEl = document.querySelector("#start-again")
+let btnClearScoresEl = document.querySelector("#clear-scores")
+
+let questionEl = document.getElementById("question")
+let answerbuttonsEl = document.getElementById("answer-buttons")
+let timerEl = document.querySelector("#timer");
+
+
+
+let questions = [
+    
+]
